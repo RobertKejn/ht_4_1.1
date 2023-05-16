6c227dd6c106bf67ac28cb947a6d4f07c8dd1ed6
Raymond <robertkejn@gmail,com>
2023-05-16 07:10:12 +0400

    Initial Commit

---
+++ ht_4_1.1/ht_4_1.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <thread>
+#include <chrono>
+#include <iostream>
+
+int maxClient = 5;
+
+void client(int& load) {
+    while (load < maxClient) {
+        ++load;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << load << " +1 Client\n";
+    }
+}
+
+void orerationist(int& load) {
+    while (load > 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+        --load;
+        std::cout << load << " -1 Client\n";
+    }
+}
+
+
+int main()
+{
+    int load = 0;
+    
+    std::thread t1(client, std::ref(load));
+    std::thread t2(orerationist, std::ref(load));
+    t1.detach();
+    if (t2.joinable()) t2.join();
+    std::cout << "All clients are served";
+}
